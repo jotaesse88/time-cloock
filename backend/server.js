f33697f9c854452e2393ada583a3a23d4ef04852
jotaesse88 <juliosarney11@icloud.com>
2025-12-05 08:52:28 -0300

    Fix: usar process.env.PORT para Render

--- backend/server.js
+++ backend/server.js
@@ -180,5 +180,6 @@ app.get("/", (req, res) => {
 });
 
 // =====================================
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
+
